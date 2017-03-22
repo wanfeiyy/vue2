c9cf743c340136ca2e099f76a2d32cbb6c8531da
use2 <user2@163.com>
2017-03-23 00:24:43 +0800

    msite rating

--- src/components/common/shoplist.vue
+++ src/components/common/shoplist.vue
@@ -14,6 +14,21 @@
                             <li v-for="item in item.supports" :key="item.id" class="supports"> {{item.icon_name}}</li>
                         </ul>
                     </header>
+                    <h5 class="rating-order-num">
+                        <section class="rating-order-num-left">
+                            <section class="rating-section">
+                                <rating-star :rating='item.rating'></rating-star>
+                                <span class="rating-num">{{item.rating}}</span>
+                            </section>
+                            <section class="order-section">
+                                 月售{{item.recent_order_num}}单
+                            </section>
+                        </section>
+                        <section class="rating-order-num-right" v-if="item.delivery_mode">
+                            <span class="delivery-style delivery-left">{{item.delivery_mode.text}}</span>
+                            <span class="delivery-style delivery_right">准时达</span>
+                        </section>
+                    </h5>
                 </hgroup>
             </router-link>
         </ul>
@@ -50,8 +65,8 @@
             .premium::before {
                 content: '品牌';
                 display: inline-block;
-                font-size: .5rem;
-                line-height: .6rem;
+                font-size: .3rem;
+                line-height: 1rem;
                 background-color: #ffd930;
                 padding: 0 0.1rem;
                 border-radius: 0.1rem;
@@ -71,9 +86,48 @@
         }
 
     }
+    .rating-order-num {
+        @include fj(space-between);
+        height: 0.6rem;
+        margin-top: 0.52rem;
+        .rating-order-num-left {
+            @include fj(flex-start);
+            .rating-section {
+                display: flex;
+                .rating-num {
+                    @include sc(0.4rem, #ff6000);
+                    margin: 0 .2rem;
+                }
+            }
+            .order-section {
+                @include sc(0.4rem,#666);
+            }
+        }
+        .rating-order-num-right {
+            display: flex;
+            align-items: center;
+            .delivery-style {
+                font-size: .4rem;
+                padding: .04rem .08rem 0;
+                border-radius: .08rem;
+                margin-left: .08rem;
+                border: 1px;
+            }
+            .delivery-left {
+                color: #fff;
+                background-color: $blue;
+                border: 0.025rem solid $blue;
+            }
+            .delivery-right {
+                color: $blue;
+                border: 0.025rem solid $blue;
+            }
+        }
+    }
 </style>
 <script>
     import {mapState} from 'vuex'
+    import ratingStar from './ratingStar'
     import {shopList} from '../../service/getData'
     import {getImgPath} from './mixin'
     export default {
@@ -82,6 +136,9 @@
                 shopListArr: [],
                 offset: 0, // 批次加载店铺列表，每次加载20个 limit = 20
             }
+        },
+        components: {
+            ratingStar
         },
         mounted() {
             this.initData()
