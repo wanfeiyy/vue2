b358aa28c5a039df7a0936ec9ce4bdd53e150fcb
use2 <user2@163.com>
2017-04-26 00:23:39 +0800

    shop detail listView

--- src/page/shop/shop.vue
+++ src/page/shop/shop.vue
@@ -25,7 +25,8 @@
                             </svg>
                         </section>
                     </router-link>
-                    <footer class="description-footer" v-if="shopDetailData.activities.length" @click="showActivitiesFun">
+                    <footer class="description-footer" v-if="shopDetailData.activities.length"
+                            @click="showActivitiesFun">
                         <p class="ellipsis">
                             <span class="tip-icon"
                                   :style="{backgroundColor: '#' + shopDetailData.activities[0].icon_color, borderColor: '#' + shopDetailData.activities[0].icon_color}">
@@ -53,7 +54,8 @@
                         <header class="activities-title-style"><span>优惠信息</span></header>
                         <ul>
                             <li v-for="item in shopDetailData.activities" :key="item.id">
-                                <span class="activities-icon" :style="{backgroundColor:'#' + item.icon_color,borderColor:'#' + item.icon_color}">
+                                <span class="activities-icon"
+                                      :style="{backgroundColor:'#' + item.icon_color,borderColor:'#' + item.icon_color}">
                                     {{item.icon_name}}
                                 </span>
                                 <span>{{item.description}}（APP专享）</span>
@@ -107,11 +109,46 @@
                                             <strong class="menu-item-description">{{item.description}}</strong>
                                         </section>
                                         <span class="menu-detail-header-right" @click="showTitleDetail(index)"></span>
-                                        <p class="description-tip" v-if="index == TitleDetailIndex" @click="NoneTitleDetail">
+                                        <p class="description-tip" v-if="index == TitleDetailIndex"
+                                           @click="NoneTitleDetail">
                                             <span>{{item.name}}</span>
                                             {{item.description}}
                                         </p>
                                     </header>
+                                    <section v-for="(foods,foodindex) in item.foods" :key="foodindex"
+                                             class="menu-detail-list">
+                                        <router-link :to="{path:'shop/foodDetail',query:{image_path:foods.image_path,
+                                        description:foods.description,month_sales: foods.month_sales, name: foods.name,
+                                        rating: foods.rating, rating_count: foods.rating_count, satisfy_rate: foods.satisfy_rate,
+                                        foods, shopId}}" tag="div" class="menu-detail-link">
+                                            <section class="menu-food-img">
+                                                <img :src="getImgPath(foods.image_path)">
+                                            </section>
+                                            <section class="menu-food-description">
+                                                <h3 class="food-description-head">
+                                                    <strong class="description-foodname">{{foods.name}}</strong>
+                                                    <ul v-if="foods.attributes.length" class="attributes-ul">
+                                                        <li v-for="(attribute, foodindex) in foods.attributes"
+                                                            :key="foodindex"
+                                                            :style="{color: '#' + attribute.icon_color,borderColor:'#' +attribute.icon_color}"
+                                                            :class="{attribute_new: attribute.icon_name == '新'}">
+                                                            <p :style="{color: attribute.icon_name == '新'? '#fff' : '#' + attribute.icon_color}">
+                                                                {{attribute.icon_name == '新'? '新品':attribute.icon_name}}
+                                                            </p>
+                                                        </li>
+                                                    </ul>
+                                                </h3>
+                                                <p class="food-description-content">{{foods.description}}</p>
+                                                <p class="food-description-sale-rating">
+                                                    <span>月售{{foods.month_sales}}份</span>
+                                                    <span>好评率{{foods.satisfy_rate}}%</span>
+                                                </p>
+                                                <p v-if="foods.activity" class="food-activity">
+                                                    <span :style="{color: '#' + foods.activity.image_text_color,borderColor:'#' +foods.activity.icon_color}">{{foods.activity.image_text}}</span>
+                                                </p>
+                                            </section>
+                                        </router-link>
+                                    </section>
                                 </li>
                             </ul>
                         </section>
@@ -123,7 +160,7 @@
 </template>
 <script>
     import {mapState, mapMutations} from 'vuex'
-    import {shopDetails,msiteAdress,foodMenu} from '../../service/getData'
+    import {shopDetails, msiteAdress, foodMenu} from '../../service/getData'
     import ratingStar from '../../components/common/ratingStar'
     import {getImgPath} from '../../components/common/mixin'
     export default{
@@ -157,9 +194,9 @@
         mixins: [getImgPath],
         computed: {
             ...mapState([
-                'latitude','longitude'
+                'latitude', 'longitude'
             ]),
-            promotionInfo: function (){
+            promotionInfo: function () {
                 return this.shopDetailData.promotion_info || '欢迎光临，用餐高峰期请提前下单，谢谢。'
             },
         },
@@ -169,7 +206,7 @@
             ]),
             // 初始化时获取基本数据
             async initData() {
-                if (! this.latitude) {
+                if (!this.latitude) {
                     //获取位置信息
                     let res = await msiteAdress(this.geohash);
                     // 记录当前经度纬度进入vuex
@@ -183,7 +220,7 @@
             },
             // 控制活动详情页的显示隐藏
             showActivitiesFun(){
-                this.showActivities = ! this.showActivities;
+                this.showActivities = !this.showActivities;
             },
             // 商品、评论切换状态
             changeShowType: value => {
@@ -212,6 +249,7 @@
 </script>
 <style lang="scss" scoped>
     @import "../../style/mixin";
+
     .shop-container {
         display: flex;
         flex-direction: column;
@@ -220,6 +258,7 @@
         left: 0;
         z-index: 9;
     }
+
     .shop-detail-header {
         overflow: hidden;
         position: relative;
@@ -234,7 +273,7 @@
         .description-header {
             position: relative;
             z-index: 10;
-            background-color: rgba(119,103,137,.43);
+            background-color: rgba(119, 103, 137, .43);
             padding: 0.4rem 0 0.4rem 0.4rem;
             width: 100%;
             overflow: hidden;
@@ -243,7 +282,7 @@
                 .description-left {
                     margin-right: .3rem;
                     img {
-                        @include wh(2.9rem,2.9rem);
+                        @include wh(2.9rem, 2.9rem);
                         display: block;
                         border-radius: 0.15rem;
                     }
@@ -275,12 +314,12 @@
                 @include fj;
                 margin-top: 0.5rem;
                 padding-right: 1rem;
-                p{
+                p {
                     @include sc(.5rem, #fff);
-                    span{
+                    span {
                         color: #fff;
                     }
-                    .tip_icon{
+                    .tip_icon {
                         padding: 0 .04rem;
                         border: 0.025rem solid #fff;
                         border-radius: 0.1rem;
@@ -291,7 +330,7 @@
                 .ellipsis {
                     width: 84%;
                 }
-                .footer-arrow{
+                .footer-arrow {
                     @include wh(.45rem, .45rem);
                     position: absolute;
                     right: .3rem;
@@ -312,7 +351,7 @@
         padding: 1.125rem;
         .activities-shoptitle {
             text-align: center;
-            @include sc(.8rem,#fff);
+            @include sc(.8rem, #fff);
         }
         .activities-ratingstar {
             display: flex;
@@ -323,7 +362,7 @@
         .activities-list {
             margin-top: 1.5rem;
             margin-bottom: 1rem;
-            @include sc(.5rem,#fff);
+            @include sc(.5rem, #fff);
             li {
                 margin-bottom: .2rem;
                 .activities-icon {
@@ -331,7 +370,8 @@
                     display: inline-block;
                     border: .025rem solid #fff;
                     border-radius: .1rem;
-                };
+                }
+            ;
                 span {
                     color: #fff;
                     line-height: .6rem;
@@ -341,14 +381,14 @@
         .activities-shopinfo {
             p {
                 line-height: .7rem;
-                @include sc(.5rem,#fff)
+                @include sc(.5rem, #fff)
             }
         }
         .activities-title-style {
             text-align: center;
             margin-bottom: 1rem;
             span {
-                @include sc(.5rem,#fff);
+                @include sc(.5rem, #fff);
                 border: 0.025rem solid #555;
                 padding: .2rem .4rem;
                 border-radius: 0.5rem;
@@ -369,7 +409,7 @@
             flex: 1;
             text-align: center;
             span {
-                @include sc(.65rem,#666);
+                @include sc(.65rem, #666);
                 padding: .2rem .1rem;
                 border-bottom: 0.12rem solid #fff;
             }
@@ -385,6 +425,7 @@
         flex: 1;
         padding-bottom: 2rem;
     }
+
     .menu-container {
         display: flex;
         flex: 1;
@@ -399,10 +440,10 @@
                 border-left: 0.15rem solid #f8f8f8;
                 position: relative;
                 img {
-                    @include wh(.5rem,.6rem);
+                    @include wh(.5rem, .6rem);
                 }
                 span {
-                    @include sc(.6rem,#666);
+                    @include sc(.6rem, #666);
                 }
                 .category-num {
                     position: absolute;
@@ -415,8 +456,8 @@
                     border: 0.025rem solid #ff461d;
                     min-width: .6rem;
                     height: .6rem;
-                    @include sc(.5rem,#fff);
-                    font-family: Helvetica Neue,Tahoma,Arial;
+                    @include sc(.5rem, #fff);
+                    font-family: Helvetica Neue, Tahoma, Arial;
                 }
             }
 
@@ -455,7 +496,7 @@
                     }
                 }
                 .menu-detail-header-right {
-                    @include wh(.5rem,.5rem);
+                    @include wh(.5rem, .5rem);
                     display: block;
                     @include bis('../../images/icon_point.png');
                     background-size: 100% .4rem;
@@ -473,7 +514,7 @@
                     padding: .5rem .4rem;
                     border: 1px;
                     border-radius: .2rem;
-                    span{
+                    span {
                         color: #fff;
                         line-height: .6rem;
                         font-size: .55rem;
@@ -489,6 +530,74 @@
                     transform: rotate(-45deg) translateY(.41rem);
                 }
             }
+            .menu-detail-list {
+                background-color: #fff;
+                padding: .6rem .4rem;
+                border-bottom: 1px solid #f8f8f8;
+                position: relative;
+                overflow: hidden;
+                .menu-detail-link {
+                    display: flex;
+                    .menu-food-img {
+                        margin-right: .4rem;
+                        img {
+                            @include wh(2rem,2rem);
+                            display: block;
+                        }
+                    }
+                    .menu-food-description {
+                        width: 100%;
+                        .food-description-head {
+                            @include fj;
+                            margin-bottom: .2rem;
+                            align-items: center;
+                            .description-foodname {
+                                @include sc(.7rem,#333);
+                            }
+                            .attributes-ul {
+                                display: flex;
+                                li {
+                                    font-size: .3rem;
+                                    height: .6rem;
+                                    line-height: .35rem;
+                                    padding: .1rem;
+                                    border: 1px solid #666;
+                                    border-radius: 0.3rem;
+                                    margin-right: .1rem;
+                                    transform: scale(.8);
+                                    p{
+                                        white-space: nowrap;
+                                        line-height: .4rem;
+                                    }
+                                }
+                            }
+                        }
+                        .food-description-content {
+                            @include sc(.5rem,#999);
+                            line-height: .6rem;
+                        }
+                        .food-description-sale-rating {
+                            line-height: .8rem;
+                            span {
+                                @include sc(.5rem,#333);
+                            }
+                        }
+                        .food-activity {
+                            line-height: .4rem;
+                            span {
+                                font-size: .3rem;
+                                border: 1px solid currentColor;
+                                border-radius: 0.3rem;
+                                padding: .08rem;
+                                display: inline-block;
+                                transform: scale(.8);
+                                margin-left: -0.35rem;
+                            }
+                        }
+                    }
+                }
+            }
         }
+
     }
 </style>
